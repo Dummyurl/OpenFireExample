55602b9ad7af9d380f3ff50d3106d3cac0c1632b
renmingjian <769091349@qq.com>
2017-12-15 16:00:42 +0800

    添加Smack的注册

--- app/src/main/java/com/jj/investigation/openfire/activity/RegisterActivity.java
+++ app/src/main/java/com/jj/investigation/openfire/activity/RegisterActivity.java
@@ -13,15 +13,15 @@ import com.jj.investigation.openfire.XmppManager;
 import com.jj.investigation.openfire.utils.ToastUtils;
 import com.jj.investigation.openfire.view.LoadingDialog;
 
-import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smackx.iqregister.AccountManager;
 
 /**
- * 登录
+ * 注册
  * Created by ${R.js} on 2017/12/15.
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     private EditText et_username;
     private EditText et_password;
@@ -30,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         initView();
         loadingDialog = new LoadingDialog(this);
     }
@@ -40,47 +40,49 @@ public class LoginActivity extends AppCompatActivity {
         et_password = (EditText) findViewById(R.id.et_password);
     }
 
-    public void login(View v) {
-        LoginTask loginTask = new LoginTask();
-        loginTask.execute(et_username.getText().toString(), et_password
+    /**
+     * 注册
+     */
+    public void register(View v) {
+        RegisterTask registerTask = new RegisterTask();
+        registerTask.execute(et_username.getText().toString(), et_password
                 .getText().toString());
     }
 
-    /**
-     * 登录任务
-     */
-    private class LoginTask extends AsyncTask<String, Void, Boolean> {
+    class RegisterTask extends AsyncTask<String, Void, Boolean> {
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            loadingDialog.showDialog("正在登录...");
+            loadingDialog.showDialog("正在注册...");
         }
 
         @Override
         protected Boolean doInBackground(String... params) {
+            //新版本注册
             XMPPTCPConnection connection = XmppManager.getConnection();
+            final AccountManager accountManager = AccountManager.getInstance(connection);
             try {
-                connection.login(params[0], params[1]);
-                connection.sendStanza(new Presence(Presence.Type.available));
+                accountManager.createAccount(params[0], params[1]);
             } catch (Exception e) {
                 e.printStackTrace();
-                Log.e("登录失败：", e.toString());
+                Log.e("注册失败：", e.toString());
                 return false;
             }
             return true;
         }
 
         @Override
-        protected void onPostExecute(Boolean loginStatus) {
+        protected void onPostExecute(Boolean registerStatus) {
+            super.onPostExecute(registerStatus);
             loadingDialog.hideDialog();
-            if (loginStatus) {
-                ToastUtils.showShortToastSafe("登录成功");
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            if (registerStatus) {
+                ToastUtils.showShortToastSafe("注册成功");
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
             } else {
-                ToastUtils.showShortToastSafe("登录失败");
+                ToastUtils.showShortToastSafe("注册失败");
             }
         }
-    }
 
+    }
 }
